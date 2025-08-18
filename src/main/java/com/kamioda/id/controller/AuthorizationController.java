abfecb6946524d50ba0cdfc05675499eca748ef9
Kamioda <kamiodamasato@outlook.com>
2025-08-18 18:08:03 +0900

    Controller->RestController

--- src/main/java/com/kamioda/id/controller/AuthorizationController.java
+++ src/main/java/com/kamioda/id/controller/AuthorizationController.java
@@ -6,17 +6,17 @@ import java.util.Map;
 import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.kamioda.id.service.AuthorizationService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
-@Controller(value = "oauth")
+@RestController(value = "oauth")
 public class AuthorizationController extends ControllerBase {
     @Autowired
     private AuthorizationService authorizationService;
