53cdefa7b8f51060834b894f7ecc6812051740f9
Kamioda <kamiodamasato@outlook.com>
2025-09-04 17:59:32 +0900

    アクセストークンのrevoke endpointの追加

--- src/main/java/com/kamioda/id/controller/AuthorizationController.java
+++ src/main/java/com/kamioda/id/controller/AuthorizationController.java
@@ -2,10 +2,11 @@ package com.kamioda.id.controller;
 
 import java.net.URI;
 import java.util.Map;
-
+import com.kamioda.id.service.TokenService;
 import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,58 +18,79 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-
 @RestController(value = "oauth")
 public class AuthorizationController extends ControllerBase {
     @Autowired
+    private TokenService tokenService;
+    @Autowired
     private AuthorizationService authorizationService;
+
+    AuthorizationController(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
     @GetMapping("/oauth/authorize")
     public ResponseEntity<?> startAuthorization(
-        @RequestParam("response_type") String responseType,
-        @RequestParam("client_id") String clientId, 
-        @RequestParam("redirect_uri") String redirectUri,
-        @RequestParam("code_challenge") String codeChallenge,
-        @RequestParam("code_challenge_method") String codeChallengeMethod
-    ) {
+            @RequestParam("response_type") String responseType,
+            @RequestParam("client_id") String clientId,
+            @RequestParam("redirect_uri") String redirectUri,
+            @RequestParam("code_challenge") String codeChallenge,
+            @RequestParam("code_challenge_method") String codeChallengeMethod) {
         try {
-            if (responseType != "code") throw new BadRequestException("Invalid response type");
+            if (responseType != "code")
+                throw new BadRequestException("Invalid response type");
             if (clientId == null || redirectUri == null || codeChallenge == null || codeChallengeMethod == null) {
                 throw new BadRequestException("Missing required parameters");
             }
-            String authId = authorizationService.createAuthorization(clientId, redirectUri, codeChallenge, codeChallengeMethod);
-            return ResponseEntity.status(302).location(new URI("https://id.kamioda.tokyo/authorize?auth_id=" + authId)).build();
+            String authId = authorizationService.createAuthorization(clientId, redirectUri, codeChallenge,
+                    codeChallengeMethod);
+            return ResponseEntity.status(302).location(new URI("https://id.kamioda.tokyo/authorize?auth_id=" + authId))
+                    .build();
         } catch (Exception e) {
             return exceptionProcess(e);
         }
     }
+
     @PostMapping("/oauth/authorize/{authId}")
     public ResponseEntity<?> authorize(@PathVariable String authId, @RequestBody Map<String, String> body) {
         try {
-            String redirectUri = authorizationService.authorizationWithIDAndPassword(authId, body.get("id"), body.get("password"));
+            String redirectUri = authorizationService.authorizationWithIDAndPassword(authId, body.get("id"),
+                    body.get("password"));
             return ResponseEntity.status(302).location(URI.create(redirectUri)).build();
         } catch (Exception e) {
             return exceptionProcess(e);
         }
     }
+
     @PostMapping("/oauth/token")
-    public ResponseEntity<?> issueToken(@RequestBody Map<String, String> body, @RequestHeader("Authorization") String authHeader) {
+    public ResponseEntity<?> issueToken(@RequestBody Map<String, String> body,
+            @RequestHeader("Authorization") String authHeader) {
         try {
             AppAuthorization appAuthInfo = new AppAuthorization(authHeader);
-            switch(body.get("grant_type")) {
+            switch (body.get("grant_type")) {
                 case "authorization_code":
                     return ResponseEntity.ok(authorizationService.issueToken(
-                        body.get("auth_code"),
-                        body.get("code_verifier"),
-                        body.get("redirect_uri"),
-                        appAuthInfo
-                    ));
+                            body.get("auth_code"),
+                            body.get("code_verifier"),
+                            body.get("redirect_uri"),
+                            appAuthInfo));
                 case "refresh_token":
                     return ResponseEntity.ok(authorizationService.issueToken(
-                        body.get("refresh_token")
-                    ));
+                            body.get("refresh_token")));
                 default:
                     throw new BadRequestException("Invalid grant type");
             }
+        } catch (Exception e) {
+            return exceptionProcess(e);
+        }
+    }
+
+    @DeleteMapping("/oauth/token")
+    public ResponseEntity<?> revokeToken(@RequestHeader("Authorization") String authHeader) {
+        try {
+            String accessToken = pickupAccessToken(authHeader);
+            tokenService.revokeToken(accessToken);
+            return ResponseEntity.noContent().build();
         } catch (Exception e) {
             return exceptionProcess(e);
         }
